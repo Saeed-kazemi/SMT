4fba54ebbfa011f6cc8e1f612f963ec23000e5fe
s2-kazemikhiya <saeed2.kazemi-khiyabani@live.uwe.ac.uk>
2022-03-12 00:56:39 +0000

    Front-end init
    Starting to work on the Front-end

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,56 @@
+import React, { Fragment, useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    // [e.target.name] to be able to get the name value of every filed
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Log in Success');
+  };
+
+  return (
+    <section className='container'>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Sign Into Your Account
+      </p>
+      <form className='form' onSubmit={onSubmit}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={onChange}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </section>
+  );
+};
+
+export default Login;
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link, Navigate } from 'react-router-dom';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Stakeholder Mapping</h1>
+          <p className='lead'>
+            Create a buiness profile, share posts, get help and find other
+            bsuinesses to colabrate with
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
