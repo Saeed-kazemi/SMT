6fe765a42d6be973ac85379bebdc3984679a68d5
Saeed Kazemi <saeedkazemi.k@gmail.com>
2022-03-23 19:46:08 +0000

    Fixed error in ProfileForm.js

--- client/src/components/profile-forms/ProfileForm.js
+++ client/src/components/profile-forms/ProfileForm.js
@@ -81,165 +81,164 @@ const ProfileForm = ({
   };
 
   return (
+    <section className='container'>
+      <h1 className='large text-primary'>
+        {creatingProfile ? 'Create Your Profile' : 'Edit Your Profile'}
+      </h1>
+      <p className='lead'>
+        <i className='fas fa-user' />
+        {creatingProfile
+          ? ` Let's get some information to make your`
+          : ' Add some changes to your profile'}
+      </p>
+      <small>* = required field</small>
+      <form className='form' onSubmit={onSubmit}>
+        <div className='form-group'>
+          <select name='status' value={status} onChange={onChange}>
+            <option>* Select Professional Status</option>
+            <option value='Space'>Space</option>
+            <option value='Defence'>Defence</option>
+            <option value='Aerospace'>Aerospace</option>
+            <option value='Aviation'>Aviation</option>
 
-      <section className='container'>
-        <h1 className='large text-primary'>
-          {creatingProfile ? 'Create Your Profile' : 'Edit Your Profile'}
-        </h1>
-        <p className='lead'>
-          <i className='fas fa-user' />
-          {creatingProfile
-            ? ` Let's get some information to make your`
-            : ' Add some changes to your profile'}
-        </p>
-        <small>* = required field</small>
-        <form className='form' onSubmit={onSubmit}>
-          <div className='form-group'>
-            <select name='status' value={status} onChange={onChange}>
-              <option>* Select Professional Status</option>
-              <option value='Space'>Space</option>
-              <option value='Defence'>Defence</option>
-              <option value='Aerospace'>Aerospace</option>
-              <option value='Aviation'>Aviation</option>
+            <option value='Other'>Other</option>
+          </select>
+          <small className='form-text'>
+            Give us an idea of what field you are working on mostly
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Company'
+            name='company'
+            value={company}
+            onChange={onChange}
+          />
+          <small className='form-text'>Company name</small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Website'
+            name='website'
+            value={website}
+            onChange={onChange}
+          />
+          <small className='form-text'>Company website</small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Location'
+            name='location'
+            value={location}
+            onChange={onChange}
+          />
+          <small className='form-text'>City(eg. Bristol)</small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='* Skills'
+            name='skills'
+            value={skills}
+            onChange={onChange}
+          />
+          <small className='form-text'>
+            Please use comma separated values (eg. System engineering,
+            requirements analysis, Modeling)
+          </small>
+        </div>
+        <div className='form-group'>
+          <textarea
+            placeholder='A short bio of the company'
+            name='bio'
+            value={bio}
+            onChange={onChange}
+          />
+          <small className='form-text'>
+            Tell us a little about the company
+          </small>
+        </div>
 
-              <option value='Other'>Other</option>
-            </select>
-            <small className='form-text'>
-              Give us an idea of what field you are working on mostly
-            </small>
-          </div>
-          <div className='form-group'>
-            <input
-              type='text'
-              placeholder='Company'
-              name='company'
-              value={company}
-              onChange={onChange}
-            />
-            <small className='form-text'>Company name</small>
-          </div>
-          <div className='form-group'>
-            <input
-              type='text'
-              placeholder='Website'
-              name='website'
-              value={website}
-              onChange={onChange}
-            />
-            <small className='form-text'>Company website</small>
-          </div>
-          <div className='form-group'>
-            <input
-              type='text'
-              placeholder='Location'
-              name='location'
-              value={location}
-              onChange={onChange}
-            />
-            <small className='form-text'>City(eg. Bristol)</small>
-          </div>
-          <div className='form-group'>
-            <input
-              type='text'
-              placeholder='* Skills'
-              name='skills'
-              value={skills}
-              onChange={onChange}
-            />
-            <small className='form-text'>
-              Please use comma separated values (eg. System engineering,
-              requirements analysis, Modeling)
-            </small>
-          </div>
-          <div className='form-group'>
-            <textarea
-              placeholder='A short bio of the company'
-              name='bio'
-              value={bio}
-              onChange={onChange}
-            />
-            <small className='form-text'>
-              Tell us a little about the company
-            </small>
-          </div>
+        <div className='my-2'>
+          <button
+            onClick={() => toggleSocialInputs(!displaySocialInputs)}
+            type='button'
+            className='btn btn-light'
+          >
+            Add Social Network Links
+          </button>
+          <span>Optional</span>
+        </div>
 
-          <div className='my-2'>
-            <button
-              onClick={() => toggleSocialInputs(!displaySocialInputs)}
-              type='button'
-              className='btn btn-light'
-            >
-              Add Social Network Links
-            </button>
-            <span>Optional</span>
-          </div>
+        {displaySocialInputs && (
+          <Fragment>
+            <div className='form-group social-input'>
+              <i className='fab fa-twitter fa-2x' />
+              <input
+                type='text'
+                placeholder='Twitter URL'
+                name='twitter'
+                value={twitter}
+                onChange={onChange}
+              />
+            </div>
 
-          {displaySocialInputs && (
-            <Fragment>
-              <div className='form-group social-input'>
-                <i className='fab fa-twitter fa-2x' />
-                <input
-                  type='text'
-                  placeholder='Twitter URL'
-                  name='twitter'
-                  value={twitter}
-                  onChange={onChange}
-                />
-              </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-facebook fa-2x' />
+              <input
+                type='text'
+                placeholder='Facebook URL'
+                name='facebook'
+                value={facebook}
+                onChange={onChange}
+              />
+            </div>
 
-              <div className='form-group social-input'>
-                <i className='fab fa-facebook fa-2x' />
-                <input
-                  type='text'
-                  placeholder='Facebook URL'
-                  name='facebook'
-                  value={facebook}
-                  onChange={onChange}
-                />
-              </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-youtube fa-2x' />
+              <input
+                type='text'
+                placeholder='YouTube URL'
+                name='youtube'
+                value={youtube}
+                onChange={onChange}
+              />
+            </div>
 
-              <div className='form-group social-input'>
-                <i className='fab fa-youtube fa-2x' />
-                <input
-                  type='text'
-                  placeholder='YouTube URL'
-                  name='youtube'
-                  value={youtube}
-                  onChange={onChange}
-                />
-              </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-linkedin fa-2x' />
+              <input
+                type='text'
+                placeholder='Linkedin URL'
+                name='linkedin'
+                value={linkedin}
+                onChange={onChange}
+              />
+            </div>
 
-              <div className='form-group social-input'>
-                <i className='fab fa-linkedin fa-2x' />
-                <input
-                  type='text'
-                  placeholder='Linkedin URL'
-                  name='linkedin'
-                  value={linkedin}
-                  onChange={onChange}
-                />
-              </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-instagram fa-2x' />
+              <input
+                type='text'
+                placeholder='Instagram URL'
+                name='instagram'
+                value={instagram}
+                onChange={onChange}
+              />
+            </div>
+          </Fragment>
+        )}
 
-              <div className='form-group social-input'>
-                <i className='fab fa-instagram fa-2x' />
-                <input
-                  type='text'
-                  placeholder='Instagram URL'
-                  name='instagram'
-                  value={instagram}
-                  onChange={onChange}
-                />
-              </div>
-            </Fragment>
-          )}
-
-          <input type='submit' className='btn btn-primary my-1' />
-          <Link className='btn btn-light my-1' to='/dashboard'>
-            Go Back
-          </Link>
-        </form>
-      </section>
-s  );
+        <input type='submit' className='btn btn-primary my-1' />
+        <Link className='btn btn-light my-1' to='/dashboard'>
+          Go Back
+        </Link>
+      </form>
+    </section>
+  );
 };
 
 ProfileForm.propTypes = {
